1683a36a1fa186e6a460e27e8c72299351a1c617
IchwanulFata <101862614+IchwanulFata@users.noreply.github.com>
2022-09-12 14:13:45 +0700

    Create prak2soal1.java

---
+++ Praktikum 2/prak2soal1.java
@@ -0,0 +1,12 @@
+public class prak2soal1{
+    public static void main(String args[]){
+        int num1 = 3;
+        int num2 = 2;
+
+        System.out.println("num1 + num2 = " + (num1+num2));
+        System.out.println("num1 - num2 = " + (num1-num2));
+        System.out.println("num1 * num2 = " + (num1*num2));
+        System.out.println("num1 / num2 = " + (float)((float)num1/(float)num2));
+        System.out.println("num1 % num2 = " + (num1%num2));
+    }
+}
